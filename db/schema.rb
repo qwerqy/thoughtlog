8e5c8107ab337a93547185c9bfafd01fe8a6a062
qwerqy <amnrsln@gmail.com>
2018-10-23 20:09:51 +0800

    user database modify

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_23_112300) do
+ActiveRecord::Schema.define(version: 2018_10_23_120706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,9 +21,9 @@ ActiveRecord::Schema.define(version: 2018_10_23_112300) do
     t.string "email"
     t.string "about"
     t.string "location"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
