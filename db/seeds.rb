db9b587893503dbffdd202a4734b6b524b540502
qwerqy <amnrsln@gmail.com>
2018-10-24 20:33:56 +0800

    navbar fixes. seeds....later

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,12 +4,12 @@ user['password'] = 'asdf'
 # user['password_confirmation'] = 'asdf'
 
 ActiveRecord::Base.transaction do
-  10.times do
+  20.times do
     user['first_name'] = Faker::Name.first_name
     user['last_name'] = Faker::Name.last_name
     user['email'] = Faker::Internet.email
     user['about'] = Faker::Lorem.paragraph(10)
-    user['location'] = Faker::Nation.capital_city
+    user['location'] = Faker::Address.city
     user['remote_avatar_url'] = Faker::LoremFlickr.image("300x300", ['people'])
 
     User.create(user)
