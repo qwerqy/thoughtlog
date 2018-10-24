b4cd6d4cb5d04b8663740bd0f1d8b32264a2c71c
qwerqy <amnrsln@gmail.com>
2018-10-25 00:27:22 +0800

    projects controller updated

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,8 +6,47 @@ class ProjectsController < ApplicationController
   end
 
   def new
+    @user = User.find(params[:user_id])
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @project = @user.projects.build(project_params)
+    if @project.save
+      redirect_to @user
+    else
+      flash[:error] = "Something's wrong. #{@project.errors.full_messages.to_sentence}"
+      redirect_back(fallback_location: @user)
+    end
   end
 
   def edit
+    @user = User.find(params[:user_id])
+    @project = @user.projects.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:user_id])
+    @project = @user.projects.find(params[:id])
+    @project.update(project_params)
+    redirect_to @user
+  end
+
+  def destroy
+    @user = User.find(params[:user_id])
+    @project = @user.projects.find(params[:id])
+    @project.destroy
+    redirect_back(fallback_location: @user)
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(
+      :title,
+      :description,
+      :link,
+      :photo
+    )
   end
 end
