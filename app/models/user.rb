5afdce284c3c224037a56498fc2b243d1402a511
qwerqy <amnrsln@gmail.com>
2018-10-24 18:22:09 +0800

    followings model initial state

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   include BCrypt
   has_secure_password
 
+  has_many :followers, :class_name => 'Followings', :foreign_key => 'following_id'
+  has_many :following, :class_name => 'Followings', :foreign_key => 'follower_id'
+
   validates :first_name, presence: true, if: :first_name
   validates :last_name, presence: true, if: :last_name
   validates :email, presence: true, uniqueness: true, if: :email
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_24_083242) do
+ActiveRecord::Schema.define(version: 2018_10_24_102020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "followings", force: :cascade do |t|
+    t.integer "following_id"
+    t.integer "follower_id"
+    t.boolean "blocked"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
