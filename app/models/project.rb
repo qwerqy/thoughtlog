1b38a3883dbdb277662fbe5372c0b6ae6e85d0b2
qwerqy <amnrsln@gmail.com>
2018-10-25 00:27:37 +0800

    project model updated with link validation

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,13 @@
 class Project < ApplicationRecord
+  mount_uploader :photo, PhotoUploader
+  belongs_to :user
+
+  URL_REGEXP = /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/ix
+  validates :link, format: { with: URL_REGEXP, message: 'You provided invalid URL' }, unless: :leave_empty?
+
+  private
+
+  def leave_empty?
+    link == ''
+  end
 end
