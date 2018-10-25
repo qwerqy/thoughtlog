f02742796ffc4242d769b714aad284bb87a34100
qwerqy <amnrsln@gmail.com>
2018-10-25 19:23:45 +0800

    thought g files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_24_140600) do
+ActiveRecord::Schema.define(version: 2018_10_25_110522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,16 @@ ActiveRecord::Schema.define(version: 2018_10_24_140600) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "thoughts", force: :cascade do |t|
+    t.string "content"
+    t.bigint "user_id"
+    t.bigint "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_thoughts_on_project_id"
+    t.index ["user_id"], name: "index_thoughts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -48,4 +58,6 @@ ActiveRecord::Schema.define(version: 2018_10_24_140600) do
   end
 
   add_foreign_key "projects", "users"
+  add_foreign_key "thoughts", "projects"
+  add_foreign_key "thoughts", "users"
 end
