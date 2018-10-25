a94ee8de76a8272280def8e061ea272e0a79d06b
qwerqy <amnrsln@gmail.com>
2018-10-25 20:06:51 +0800

    thought nested route

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,9 @@ Rails.application.routes.draw do
   resources :user, controller: 'users' do
     post '/follow' => 'users#follow', as: 'follow'
     delete '/unfollow' => 'users#unfollow', as: 'unfollow'
-    resources :projects, controller: 'projects', only: [:create, :new, :show, :edit, :update, :destroy]
+    resources :projects, controller: 'projects', only: [:create, :new, :show, :edit, :update, :destroy] do
+      resources :thoughts, controller: 'thoughts'
+    end
   end
 
   get '/projects/all' => 'projects#index', as: 'all_projects'
