99d641c51a49ee3030ba37314523a47a7ea1310c
qwerqy <amnrsln@gmail.com>
2018-10-25 19:22:54 +0800

    js management

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,14 +13,16 @@
 //= require rails-ujs
 //= require jquery
 //= require semantic-ui
+//= require home
 //= require activestorage
 //= require turbolinks
 //= require trix
 //= require_tree .
 
-
-$(".message.closable .close.icon").on ("click", () => {
-  $('.message.closable').fadeOut("slow")[
-  false
-  ]
+$(document).on('turbolinks:load', function() {
+  $(".message.closable .close.icon").on ("click", () => {
+    $('.message.closable').fadeOut("slow")[
+    false
+    ]
+  })
 })
