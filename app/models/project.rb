e8990e68d0bbb3b99d45a3c45bca91123b35a3ae
qwerqy <amnrsln@gmail.com>
2018-10-26 00:37:54 +0800

    adjustments to models based on tests

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,6 +5,7 @@ class Project < ApplicationRecord
 
   URL_REGEXP = /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/ix
   validates :link, format: { with: URL_REGEXP, message: 'You provided invalid URL' }, unless: :leave_empty?
+  validates_presence_of(:title)
 
   private
 
