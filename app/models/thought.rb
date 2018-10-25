a39fe8d6f7a7dacf88773ad03fc05c73a87314e4
qwerqy <amnrsln@gmail.com>
2018-10-25 21:29:22 +0800

    typo

--- app/models/thought.rb
+++ app/models/thought.rb
@@ -1,6 +1,6 @@
 class Thought < ApplicationRecord
   belongs_to :user
-  belongs_to :project, counter_cache: thoughts_count
+  belongs_to :project, counter_cache: :thoughts_count
 
   validates :content, presence: true
 end
