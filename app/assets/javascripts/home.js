8aeed89cb4a118b53e8a74c0a9b69738518a4df4
qwerqy <amnrsln@gmail.com>
2018-10-27 00:22:10 +0800

    js cleanup

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,9 +1,9 @@
 $(document).on('turbolinks:load', function() {
-  $('.image img')
-    .visibility({
-      type       : 'image',
-      transition : 'fade in',
-      duration   : 1000
-    })
-  ;
+    $('.image img')
+      .visibility({
+        type       : 'image',
+        transition : 'fade in',
+        duration   : 1000
+      })
+    ;
 })
