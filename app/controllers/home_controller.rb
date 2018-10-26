1e3ec8b589939bf5c197dc8229cacaea6ed929a0
qwerqy <amnrsln@gmail.com>
2018-10-27 00:22:52 +0800

    remove request consumer_key. added initializer to handle that

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,8 @@
 class HomeController < ApplicationController
   def index
-    client = Tumblr::Client.new consumer_key: ENV['TUMBLR_KEY']
+    client = Tumblr::Client.new
     @tagged = client.tagged'ideas', limit: 10
     @projects = Project.includes(:user).limit(20).order(created_at: :desc)
-    respond_to do |format|
-      format.html
-      format.json { render json: @tagged }
-    end
   end
 
   def show
