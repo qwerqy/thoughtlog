3acabbc185452ab07cb137c3f1a6de59f6339c26
qwerqy <amnrsln@gmail.com>
2018-10-26 19:05:42 +0800

    schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_26_093706) do
+ActiveRecord::Schema.define(version: 2018_10_26_095806) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 2018_10_26_093706) do
     t.index ["user_id"], name: "index_authentications_on_user_id"
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.bigint "project_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_likes_on_project_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
   create_table "projects", force: :cascade do |t|
     t.string "title"
     t.string "description"
@@ -34,6 +43,7 @@ ActiveRecord::Schema.define(version: 2018_10_26_093706) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.integer "thoughts_count"
+    t.integer "likes_count"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
@@ -42,15 +52,6 @@ ActiveRecord::Schema.define(version: 2018_10_26_093706) do
     t.integer "follower_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "saves", force: :cascade do |t|
-    t.bigint "project_id"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["project_id"], name: "index_saves_on_project_id"
-    t.index ["user_id"], name: "index_saves_on_user_id"
   end
 
   create_table "thoughts", force: :cascade do |t|
@@ -75,12 +76,13 @@ ActiveRecord::Schema.define(version: 2018_10_26_093706) do
     t.string "avatar"
     t.integer "followings_count"
     t.integer "followers_count"
+    t.integer "likes_count"
   end
 
   add_foreign_key "authentications", "users"
+  add_foreign_key "likes", "projects"
+  add_foreign_key "likes", "users"
   add_foreign_key "projects", "users"
-  add_foreign_key "saves", "projects"
-  add_foreign_key "saves", "users"
   add_foreign_key "thoughts", "projects"
   add_foreign_key "thoughts", "users"
 end
