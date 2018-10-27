466200c025d12a1d5e25717722c76a0c93658481
qwerqy <amnrsln@gmail.com>
2018-10-27 16:22:20 +0800

    now you are able to get inspired by flickr

--- app/controllers/inspires_controller.rb
+++ app/controllers/inspires_controller.rb
@@ -1,5 +1,5 @@
 class InspiresController < ApplicationController
-  def create
+  def tumblr_create
     client = Tumblr::Client.new
     project = client.posts "#{params[:blog_name]}.tumblr.com", :id => params[:id]
 
@@ -20,6 +20,24 @@ class InspiresController < ApplicationController
       description: description,
       photo: photo,
       link: project['posts'][0]['short_url'],
+    }
+    i = Idea.create!(idea)
+    Inspire.create!(idea_id: i.id, user_id: current_user.id)
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def flickr_create
+    @user = params[:user_name]
+    @photo_id = params[:id]
+    @project = Project.show_flickr(@photo_id)
+
+    idea = {
+      title: @project[:title],
+      description: '',
+      photo: @project[:photo],
+      link: @project[:link],
     }
     i = Idea.create!(idea)
     Inspire.create!(idea_id: i.id, user_id: current_user.id)
