a6c5861b626ee65521a542f7eaa19b80d215ee42
qwerqy <amnrsln@gmail.com>
2018-10-27 16:24:39 +0800

    added routes, and destroy idea respond

--- config/routes.rb
+++ config/routes.rb
@@ -17,11 +17,14 @@ Rails.application.routes.draw do
       resources :likes, controller: 'likes', only: [:create, :destroy]
     end
     get '/inspired-project/:id/new' => 'projects#new_inspired_projects', as: 'inspired_project'
+    delete '/inspired-project/:id/delete' => 'projects#destroy_inspired_projects', as: 'remove_inspired_project'
   end
 
   get '/projects/all' => 'projects#index', as: 'all_projects'
   get '/projects/:blog_name/:id' => 'projects#tumblr_show', as: 'tumblr'
-  post '/projects/:blog_name/:id/inspire' => 'inspires#create', as: 'idea_inspire'
+  get '/projects/flickr/:user_name/:id' => 'projects#flickr_show', as: 'flickr'
+  post '/projects/:blog_name/:id/inspire' => 'inspires#tumblr_create', as: 'tumblr_idea_inspire'
+  post '/projects/flickr/:user_name/:id/inspire' => 'inspires#flickr_create', as: 'flickr_idea_inspire'
   get '/:user_id/projects/user' => 'projects#user_projects', as: 'self_projects'
   get '/:user_id/projects/saved' => 'projects#liked_projects', as: 'liked_projects'
   get '/:user_id/ideas/inspired' => 'projects#inspired_ideas', as: 'inspired_ideas'
