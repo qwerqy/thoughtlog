3cad9883ede78509059d30fe961f9e2456993884
qwerqy <amnrsln@gmail.com>
2018-10-29 00:25:47 +0800

    modifications after test

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,13 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  it { should validate_presence_of(:title) }
+  describe 'validation' do
+    it { should validate_presence_of(:title) }
 
-  it "is not valid with invalid link format" do
-    project = described_class.new(link: 'aaaa.com' )
-    expect(project).to_not be_valid
+    describe 'link' do
+      it 'is valid without a link'
+      it 'is valid with a valid format'
+      it "is not valid with invalid link format"
+    end
   end
 
-  it { should belong_to(:user) }
-  it { should have_many(:thoughts) }
+  describe 'associations' do
+    it { should belong_to(:user) }
+    it { should have_many(:thoughts) }
+    it { should have_many(:likes) }
+  end
+
+  describe 'custom methods' do
+    describe 'self.get_flickr' do
+    end
+
+    describe 'self.show_flickr' do
+    end
+
+    describe 'self.get_tumblr' do
+    end
+
+    describe 'self.show_tumblr' do
+    end
+  end
 end
