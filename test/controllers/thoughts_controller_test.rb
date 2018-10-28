6a3c48c66afdb2e56036361234da721280eee689
qwerqy <amnrsln@gmail.com>
2018-10-28 23:06:14 +0800

    remove old test helpers

--- test/controllers/thoughts_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ThoughtsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/relationship_test.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Relationship, type: :model do
-  describe "association" do
-    it { should belong_to(:user) }
-    it { should belong_to(:follower) }
-  end
-end
