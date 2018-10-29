41f03a98eed8707e23a7502a4fa9d77cd3b443a1
qwerqy <amnrsln@gmail.com>
2018-10-29 13:03:51 +0800

    refactored codes using factory bot

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,42 +2,40 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe "validation" do
-    user = described_class.new(
-        first_name:'foo',
-        last_name: 'bar',
-        location:'thailand',
-        about:'lorem the ipsum',
-        email: 'abcd@example.com',
-        password: 'abcd1234'
-      )
 
     it { should have_secure_password }
 
     it "is valid with valid attributes" do
+      user = build(:user)
       expect(user).to be_valid
     end
 
     it "is valid without a first_name" do
+      user = build(:user)
       user.first_name = nil
       expect(user).to be_valid
     end
 
     it "is valid without a last_name" do
+      user = build(:user)
       user.last_name = nil
       expect(user).to be_valid
     end
 
     it "is valid without a location" do
+      user = build(:user)
       user.location = nil
       expect(user).to be_valid
     end
 
     it "is valid without an about" do
+      user = build(:user)
       user.about = nil
       expect(user).to be_valid
     end
 
     it "is valid without an avatar" do
+      user = build(:user)
       user.avatar = nil
       expect(user).to be_valid
     end
@@ -45,16 +43,19 @@ RSpec.describe User, type: :model do
   # EMAIL
 
     it "is not valid with invalid email format" do
+      user = build(:user)
       user.email = 'abc@example.com..'
       expect(user).to_not be_valid
     end
 
     it "should have a unique email" do
-      User.create!(first_name:'foo', last_name: 'bar', location:'thailand', about:'lorem the ipsum', email: 'abcd@example.com', password: 'abcd1234')
+      user = build(:user)
+      User.create!(first_name:'foo', last_name: 'bar', location:'thailand', about:'lorem the ipsum', email: 'johndoe@example.com', password: 'abcd1234')
       expect(user).to_not be_valid
     end
 
     it "is not valid without an email" do
+      user = build(:user)
       user.email = nil
       expect(user).to_not be_valid
     end
@@ -62,66 +63,37 @@ RSpec.describe User, type: :model do
   # PASSWORD
 
     it "is not valid without a password" do
+      user = build(:user)
       user.password = nil
       expect(user).to_not be_valid
     end
 
     it "is not valid without a minimum of 8 characters password" do
+      user = build(:user)
       user.password = 'abc233'
       expect(user).to_not be_valid
     end
   end
   # CUSSTOM METHODS
   describe "custom methods" do
-    user = described_class.new(
-      first_name:'foo',
-      last_name: 'bar',
-      location:'thailand',
-      about:'lorem the ipsum',
-      email: 'aasdasd@example.com',
-      password: 'abcd1234'
-    )
     describe 'name' do
       it "combines first and last name together" do
+        user = build(:user)
         full_name = user.name
-        expect(full_name).to eq("foo bar")
+        expect(full_name).to eq("John Doe")
       end
     end
 
     describe 'following?' do
       it "should able to return false if user is not following other user" do
-        other_user = User.create(
-          first_name:'fa',
-          last_name: 'bar',
-          location:'thailand',
-          about:'lorem the ipsum',
-          email: 'def@example.com',
-          password: 'abcd1234'
-        )
+        user = create(:user)
+        other_user = create(:user, email: 'jonnydoe@example.com')
         expect(user.following?(other_user)).to eq(nil)
       end
 
       it "should able to return true if user is following other user" do
-        user = User.new(
-          first_name:'foo',
-          last_name: 'bar',
-          location:'thailand',
-          about:'lorem the ipsum',
-          email: 'abc@example.com',
-          password: 'abcd1234'
-        )
-
-        other_user = User.new(
-          first_name:'fa',
-          last_name: 'bar',
-          location:'thailand',
-          about:'lorem the ipsum',
-          email: 'def@example.com',
-          password: 'abcd1234'
-        )
-        user.save(validate: false)
-        other_user.save(validate: false)
-
+        user = create(:user)
+        other_user = create(:user, email: 'jonnydoe@example.com')
         user.follow!(other_user)
         expect(user.following?(other_user)).to be_valid
       end
@@ -129,22 +101,9 @@ RSpec.describe User, type: :model do
 
     describe 'like?' do
       it "should return project if user likes" do
-        user = User.new(
-          first_name:'foo',
-          last_name: 'bar',
-          location:'thailand',
-          about:'lorem the ipsum',
-          email: 'abc@example.com',
-          password: 'abcd1234'
-        )
-
-        project = Project.new(
-          title: 'foo',
-          description: 'baaar'
-        )
-        user.save(validate: false)
-        project.save(validate: false)
-
+        user = create(:user)
+        other_user = create(:user, email: 'other@example.com')
+        project = create(:project, user_id: other_user.id)
         Like.create!(user_id: user.id, project_id: project.id)
         expect(user.like?(project)).to be_valid
       end
@@ -152,52 +111,16 @@ RSpec.describe User, type: :model do
 
     describe 'follow!' do
       it 'should create relationship between user 1 and user 2' do
-        user = User.new(
-          first_name:'foo',
-          last_name: 'bar',
-          location:'thailand',
-          about:'lorem the ipsum',
-          email: 'abc@example.com',
-          password: 'abcd1234'
-        )
-
-        other_user = User.new(
-          first_name:'fa',
-          last_name: 'bar',
-          location:'thailand',
-          about:'lorem the ipsum',
-          email: 'def@example.com',
-          password: 'abcd1234'
-        )
-        user.save(validate: false)
-        other_user.save(validate: false)
-
+        user = create(:user)
+        other_user = create(:user, email: 'jonnydoe@example.com')
         expect(user.follow!(other_user)).to be_valid
       end
     end
 
     describe 'unfollow' do
       it 'should unfollow user 1 and user 2' do
-        user = User.new(
-          first_name:'foo',
-          last_name: 'bar',
-          location:'thailand',
-          about:'lorem the ipsum',
-          email: 'abc@example.com',
-          password: 'abcd1234'
-        )
-
-        other_user = User.new(
-          first_name:'fa',
-          last_name: 'bar',
-          location:'thailand',
-          about:'lorem the ipsum',
-          email: 'def@example.com',
-          password: 'abcd1234'
-        )
-        user.save(validate: false)
-        other_user.save(validate: false)
-
+        user = create(:user)
+        other_user = create(:user, email: 'jonnydoe@example.com')
         user.follow!(other_user)
         expect(user.unfollow!(other_user)).to be_valid
       end
